c27a3eba178a2bbd8ac13921412eb46318ce50c1
asisamko <saulostyle.sk@gmail.com>
2026-09-04 12:40:45 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import { Elysia } from "elysia";
 
-const PORT = Number(process.env.SERVER_PORT ?? process.env.PORT ?? 3000);
-const HOST = process.env.SERVER_IP ?? "0.0.0.0";
-
 const app = new Elysia()
-  .get("/", () => ({ status: "ok" }))
+  .get("/", () => {
+    return {
+      status: "ok"
+    }
+  })
 
   .get("/update/android", async ({ query }) => {
-    const update = await Bun.file(`${import.meta.dir}/update.json`).json();
+    const update = await Bun.file("./update.json").json();
     const currentVersionCode = Number(query.versionCode ?? 0);
 
     return {
-      updateAvailable: currentVersionCode < update.versionCode,
+      updateAvailable:
+        currentVersionCode < update.versionCode,
       ...update
     };
   })
 
-  .listen({ port: PORT, hostname: HOST });
+  .listen(3000)
 
-console.log(`🦊 Elysia running on http://${app.server?.hostname}:${app.server?.port}`);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
