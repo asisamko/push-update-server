20011a90a52d165e1b91f69bde6c0611b4d9cbf1
asisamko <saulostyle.sk@gmail.com>
2026-09-04 12:38:19 +0200

    fixing ports #2

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
 import { Elysia } from "elysia";
 
 const PORT = Number(process.env.SERVER_PORT ?? process.env.PORT ?? 3000);
+const HOST = process.env.SERVER_IP ?? "0.0.0.0";
 
 const app = new Elysia()
-  .get("/", () => {
-    return {
-      status: "ok"
-    }
-  })
+  .get("/", () => ({ status: "ok" }))
 
   .get("/update/android", async ({ query }) => {
-    const update = await Bun.file("./update.json").json();
+    const update = await Bun.file(`${import.meta.dir}/update.json`).json();
     const currentVersionCode = Number(query.versionCode ?? 0);
 
     return {
-      updateAvailable:
-        currentVersionCode < update.versionCode,
+      updateAvailable: currentVersionCode < update.versionCode,
       ...update
     };
   })
 
-  .listen(3000)
+  .listen({ port: PORT, hostname: HOST });
 
-
-console.log(
-  `🦊 Elysia is running at ${PORT}`
-);
+console.log(`🦊 Elysia running on http://${app.server?.hostname}:${app.server?.port}`);
