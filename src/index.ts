5efe87c3c90cd30794cd3634dcf431e1eaa00ed2
asisamko <saulostyle.sk@gmail.com>
2026-09-04 11:20:05 +0200

    Prepare update server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => {
+    return {
+      status: "ok"
+    }
+  })
+
+  .get("/update/android", async ({ query }) => {
+    const update = await Bun.file("./update.json").json();
+    const currentVersionCode = Number(query.versionCode ?? 0);
+
+    return {
+      updateAvailable:
+        currentVersionCode < update.versionCode,
+      ...update
+    };
+  })
+
+  .listen(3000)
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
